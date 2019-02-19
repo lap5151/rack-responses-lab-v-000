d652d02f7c0fcaffe550c13149420c7248c8e408
Lauren Pardee <lap5151@gmail.com>
2019-02-19 18:29:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    resp.write "#{Time.new}\n"
+    resp.write "#{Time.new.ctime}\n"
 
-    if Time.new < 12
+    if Time.new.ctime < "12"
       resp.write "Good Morning!"
     else
       resp.write "Good Afternoon!"
