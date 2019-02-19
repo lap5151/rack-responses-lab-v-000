6f01fa91c7700e7c639427c580f9cf7cd402ce63
Lauren Pardee <lap5151@gmail.com>
2019-02-19 18:16:30 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,11 @@
+require 'pry'
 class Application
 
   def call(env)
     resp = Rack::Response.new
 
     time = Kernel.rand(1..20)
-
+    binding.pry
     resp.write "#{time}\n"
 
     if time == 12
